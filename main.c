acc881a747cc591b20588f21f883e1cbf30507d3
Oscar Hernandez <racsohlol2@gmail.com>
2019-09-14 11:02:12 -0500

    Practica6

--- main.c
+++ main.c
@@ -4,5 +4,26 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	return 0;
+	int maracuya,lagartija,amazonas;
+	printf("introduzca un numero:");
+	scanf("%d", &maracuya);
+	printf("¿desea colocar otro numero ((1)s/(2)n)?:");
+	scanf("%d", &lagartija);
+		
+	
+	 
+	while(lagartija != 2); 
+
+		printf("introduzca un numero:");
+	    scanf("%d", &amazonas);
+	
+		amazonas=maracuya+maracuya ;
+		
+		printf("¿desea colocar otro numero ((1)s/(2)n)?:");
+	    scanf("%d", &lagartija);
+	
+	  
+	
+	
+	
 }
